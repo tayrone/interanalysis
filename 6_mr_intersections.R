2b37f20a379010d0f3cd793ce4b18950b2a4ef6e
Tayrone Monteiro <tayronesm@gmail.com>
2020-06-13 17:58:28 -0300

    generate plot with no co-regulation data

--- 6_mr_intersections.R
+++ 6_mr_intersections.R
@@ -5,13 +5,15 @@
 library(reshape2)
 library(UpSetR)
 library(tidyverse)
+library(RTN)
 
-
-subgroups <- c("wnt", "shh", "g3", "g4", "g34")
+#subgroups <- c("wnt", "shh", "g3", "g4", "g34")
+subgroups <- "g34"
 
 load_mrs <- function(subgroup){
   
-  load(paste0("../expression_analysis/rdata_files/network/", subgroup, "_rtn.RData"))
+  load(paste0("../expression_analysis/rdata_files/network/", 
+              subgroup, "_rtn.RData"))
   
   assign(paste0(subgroup, "_mrs"), 
          data.frame(tf = tna.get(rtna, what = "mra")[["Regulon"]], 
@@ -22,12 +24,34 @@ mrs_list <- lapply(subgroups, load_mrs)
 
 rm(rtna, rtni, subgroups, load_mrs)
 
-#---- Wide format is required by upset method ----
+#---- Organize all data on a list ----
+
+load(paste0("../expression_analysis/rdata_files/survival/", 
+            subgroup, "_rtn.RData"))
+
+load(paste0("../methylation_analysis/control_", 
+            subgroup, "_files/rdata_files/", "2_probewise_analysed.RData"))
+
+gdata::keep(diff_methylated, mrs_list, hazardous_regulons, subgroups, sure = T)
+
+
+dm_genes <- unique(diff_methylated$hgnc_symbol)
+
+mrs_list[[1]]$analysis <- "Master Regulators"
+
+mrs_list[[2]] <- data.frame(tf = hazardous_regulons, analysis = "Hazardous Regulons")
+mrs_list[[3]] <- data.frame(tf = dm_genes, analysis = "Differentially Methylated Genes")
 
 complete_map <- do.call("rbind", mrs_list)
 
+
+#---- Wide format is required by upset method ----
+
 complete_map_wide <- dcast(complete_map, tf~analysis, length, 
                            value.var = "analysis")
+
+# only one observation is dropped out :)
+complete_map_wide <- complete_map_wide[complete.cases(complete_map_wide), ]
 
 rownames(complete_map_wide) <- complete_map_wide$tf
 complete_map_wide <- select(complete_map_wide, -tf)
