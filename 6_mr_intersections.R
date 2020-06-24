ae6b5eddf04e51b7625831fe72d22531e389959b
Tayrone Monteiro <tayronesm@gmail.com>
2020-06-24 17:34:03 -0300

    hypergeometric test for intersections of n sets, finally!

--- 6_mr_intersections.R
+++ 6_mr_intersections.R
@@ -1,8 +1,8 @@
 library(RTN)
 library(UpSetR)
 library(dplyr)
-library(vcd)
-library(rcompanion)
+library(SuperExactTest)
+library(reshape2)
 
 load("./interanalysis_files/rdata_files/5_dm_regulons.RData")
 load("../expression_analysis/rdata_files/survival/g34_survival.RData")
@@ -28,46 +28,15 @@ complete_map_wide <- select(complete_map_wide, -tf)
 
 upset(complete_map_wide, order.by = "freq")
 
+#---- Peforms the hypergeometric test for defining statistical significance of 
+# set overlapping groups ----
 
-#---- Then, check wich intersections are statistically significant,
-# using the Cochran–Mantel–Haenszel test, which is basically chi-square
-# for three dimensional tables ----
+input <- list(mrs = mrs$Regulon, hm = hm_regulons, haz = hazardous_regulons)
 
-# This will make a contigency table that adresses all combinations 
-# shown in the upsetr plot.
-count_table <- data.frame(hm = c(1, 0, 1, 1, 0, 1, 0),
-                          haz = c(0, 1, 1, 0, 0, 1, 1),
-                          mr = c(rep(0, 3), rep(1, 4)))
+result <- supertest(input, n = 1639)
 
-colnames(count_table) <- c("hm", "haz", "mr")
-
-complete_map_wide <- select(complete_map_wide, hm = `Highly Methylated Regulons`, 
-                            haz = `Hazardous Regulons`, mr = `Master Regulators`)
-
-count_table$count <- 
-  sapply(rownames(count_table), 
-        function(x) nrow(dplyr::inner_join(complete_map_wide, 
-                                           count_table[x, ])))
-
-table_mr <- xtabs(count ~ (hm + haz + mr), 
-               data = count_table)
-table_haz <- xtabs(count ~ (haz + mr + hm), 
-                  data = count_table)
-table_hm <- xtabs(count ~ (hm + mr + haz), 
-                   data = count_table)
-
-tables <- list(mr = table_mr, haz = table_haz, hm = table_hm)
-
-results <- lapply(tables, mantelhaen.test)
-
-#p-value is significant, so CMH tests are not really appropriate
-woolf_results <- lapply(tables, woolf_test)
-
-# for(i in 1:3){
-#   print(groupwiseCMH(table_mr, group = i))
-# }
-
-gdata::keep(mrs, hazardous_regulons, hm_regulons, sure = T)
-
-save.image("./interanalysis_files/rdata_files/7_hyper.RData")
-
+plot(result, Layout = "landscape", sort.by = "size", keep=FALSE,
+     bar.split = c(70,180), show.elements = F, elements.cex = 0.7,
+     elements.list = subset(summary(result)$Table, Observed.Overlap <= 10),
+     show.expected.overlap = TRUE, expected.overlap.style = "hatchedBox",
+     color.expected.overlap = 'red')
